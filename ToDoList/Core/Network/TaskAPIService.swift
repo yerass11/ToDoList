950fd1d0d698492ff7f229a4fb4ebf6d28bdb17e
yerass11 <y_saiman@kbtu.kz>
2025-07-30 09:55:51 +0500

    [feat] add network

--- ToDoList/Core/Network/TaskAPIService.swift
+++ ToDoList/Core/Network/TaskAPIService.swift
@@ -1,8 +1,40 @@
-//
-//  TaskAPIService.swift
-//  ToDoList
-//
-//  Created by Yerasyl on 29.07.2025.
-//
-
 import Foundation
+
+final class TaskAPIService {
+    func fetchTasks(completion: @escaping (Result<[Task], Error>) -> Void) {
+        guard let url = URL(string: "https://dummyjson.com/todos") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 1)))
+            return
+        }
+
+        DispatchQueue.global(qos: .background).async {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
+                    return
+                }
+
+                guard let data = data else {
+                    DispatchQueue.main.async {
+                        completion(.failure(NSError(domain: "No data", code: 2)))
+                    }
+                    return
+                }
+
+                do {
+                    let decoded = try JSONDecoder().decode(TaskListResponse.self, from: data)
+                    let tasks = decoded.todos.map { $0.toDomainModel() }
+                    DispatchQueue.main.async {
+                        completion(.success(tasks))
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
+                }
+            }.resume()
+        }
+    }
+}
