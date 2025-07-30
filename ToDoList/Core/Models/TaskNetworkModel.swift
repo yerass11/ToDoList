03a1d8b7d0cb7045b814bd58aea9f7093640b29c
yerass11 <y_saiman@kbtu.kz>
2025-07-30 09:55:34 +0500

    [feat] add models

--- ToDoList/Core/Models/TaskNetworkModel.swift
+++ ToDoList/Core/Models/TaskNetworkModel.swift
@@ -1,8 +1,20 @@
-//
-//  TaskNetworkModel.swift
-//  ToDoList
-//
-//  Created by Yerasyl on 29.07.2025.
-//
-
 import Foundation
+
+struct TaskNetworkModel: Codable {
+    let id: Int64
+    let todo: String
+    let completed: Bool
+    let userId: Int
+}
+
+extension TaskNetworkModel {
+    func toDomainModel() -> Task {
+        Task(
+            id: self.id,
+            title: self.todo,
+            description: "",
+            dateCreated: Date(),
+            isCompleted: self.completed
+        )
+    }
+}
