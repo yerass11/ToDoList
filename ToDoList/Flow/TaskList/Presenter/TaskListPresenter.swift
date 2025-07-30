c082a54d135b9ae16f61789d87668ee56db2f520
yerass11 <y_saiman@kbtu.kz>
2025-07-30 09:56:48 +0500

    [feat] add task list using VIPER

--- ToDoList/Flow/TaskList/Presenter/TaskListPresenter.swift
+++ ToDoList/Flow/TaskList/Presenter/TaskListPresenter.swift
@@ -1,12 +1,58 @@
 import Foundation
+import UIKit
 
 final class TaskListPresenter: TaskListPresenterProtocol {
+    weak var viewController: UIViewController?
     weak var view: TaskListViewProtocol?
     var interactor: TaskListInteractorProtocol?
     var router: TaskListRouterProtocol?
 
+
     func viewDidLoad() {
         interactor?.fetchTasks()
+    }
+    
+    func didTapAdd() {
+        guard let view = view as? UIViewController else { return }
+
+        router?.openNewTask(from: view) { [weak self] newTask in
+            self?.interactor?.add(with: newTask)
+        }
+    }
+    
+    func didTapTask(_ task: Task) {
+        openTaskEditor(with: task)
+    }
+    
+    func didTapEdit(_ task: Task) {
+        openTaskEditor(with: task)
+    }
+
+    func didSelectTask(_ task: Task) {
+        openTaskEditor(with: task)
+    }
+    
+    func didSwipeToDelete(task: Task) {
+        interactor?.delete(with: task)
+    }
+    
+    func didToggleTaskCompletion(_ task: Task) {
+        interactor?.toggleCompletion(with: task)
+    }
+
+    func didTapShare(_ task: Task) {
+        let activityVC = UIActivityViewController(
+            activityItems: ["Задача: \(task.title)\nОписание: \(task.description)\nДата: \(task.dateCreated)"],
+            applicationActivities: nil
+        )
+        viewController?.present(activityVC, animated: true)
+    }
+    
+    private func openTaskEditor(with task: Task) {
+        guard let view = view as? UIViewController else { return }
+        router?.openTaskDetail(from: view, task: task) { [weak self] updatedTask in
+            self?.interactor?.update(with: updatedTask)
+        }
     }
 }
 
--- ToDoList/Flow/TaskList/TaskListAssembly.swift
+++ ToDoList/Flow/TaskList/TaskListAssembly.swift
@@ -1,8 +1,22 @@
-//
-//  TaskListAssembly.swift
-//  ToDoList
-//
-//  Created by Yerasyl on 29.07.2025.
-//
+import UIKit
 
-import Foundation
+final class TaskListAssembly {
+    static func assemble() -> UIViewController {
+        let viewController = TaskListViewController()
+        let presenter = TaskListPresenter()
+        let interactor = TaskListInteractor()
+        let router = TaskListRouter()
+
+        viewController.presenter = presenter
+
+        presenter.interactor = interactor
+        presenter.view = viewController
+        presenter.viewController = viewController
+        presenter.router = router
+
+        interactor.output = presenter
+        router.viewController = viewController
+
+        return viewController
+    }
+}
